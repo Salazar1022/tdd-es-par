64c6b1bc1a0f537c456bfb5f731688a9d373053e
Sebastian Salazar Henao <157437307+Salazar1022@users.noreply.github.com>
2026-08-12 11:28:05 -0500

    Implement es_multiplo_de function
    Added function to check if a number is a multiple of another.

--- math_utils.py
+++ math_utils.py
@@ -1,14 +1,22 @@
 # Intencionalmente vacío o sin la función 'es_par' para provocar RED.
 
-# print("Ingrese un numero:")
-# n = 0
-# input(n)
-
 def es_par(n) -> bool:
     
     mod = n % 2
 
     if mod == 0:
         return True 
+    else:
+        return False
+
+def es_multiplo_de(n, m) -> bool:
+
+    if m == 0: 
+        return False
+        
+    mul = n % m
+
+    if mul == 0:
+        return True
     else:
         return False
